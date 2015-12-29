57df9d954db3ec7f5342d0a5d033546aaf29c576
NemanjaT <nemanjat94@hotmail.com>
2015-12-29 08:27:50 +0100

    node js projekat v0.6.3
    dopunjen stil, dodata funkcionalnost za dugme za vise inf.

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,16 @@
 $(function() {
-    $('')
+    var moreInfoBtn = document.getElementById('more-info-btn');
+    var searchMoreInfo = document.getElementById('search-more-info');
+
+    moreInfoBtn.addEventListener('click', function() {
+        if (searchMoreInfo.classList.contains('hidden')) {
+            searchMoreInfo.classList.remove('hidden');
+            searchMoreInfo.classList.add('shown');
+            moreInfoBtn.classList.add('rotated');
+        } else {
+            searchMoreInfo.classList.add('hidden');
+            searchMoreInfo.classList.remove('shown');
+            moreInfoBtn.classList.remove('rotated');
+        }
+    });
 });
