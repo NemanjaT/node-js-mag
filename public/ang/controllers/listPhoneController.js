7099ac56cfd86655415d92f62b1029a51993a24b
NemanjaT <nemanjat94@hotmail.com>
2016-01-03 22:37:22 +0100

    node js projekat v0.8.3

--- public/ang/controllers/listPhoneController.js
+++ public/ang/controllers/listPhoneController.js
@@ -1,6 +1,7 @@
 angular.module('cmscApp').controller('listPhoneController', ['$scope', '$http', '$location', 'searchBoxFactory',
     function($scope, $http, $location, searchBoxFactory) {
         $scope.listInfo = searchBoxFactory;
+        $scope.phoneList = [];
 
         $http.get('/api/getallphones').then(function(res) {
             $scope.phoneList = res.data;
